46c2668eda7c3ff77af6cda7f5a0015405cfa9d8
TotoEnF5 <totooodev@proton.me>
2025-06-19 15:26:05 +0200

    remove nodes

--- runtime/src/main.c
+++ runtime/src/main.c
@@ -1,22 +1,6 @@
 #include "app.h"
-#include "node.h"
-#include "nodes/root.h"
-#include "nodes/transform.h"
-#include "log.h"
-
-void f(node_t* node) {
-    PEAR_INFO("%s", node->name);
-}
 
 int main(int argc, char* argv[]) {
-    root_t* root = root_new("root");
-    transform_t* transform = node_child(root, transform_t, "transform1");
-    transform_t* t2 = node_child(transform, transform_t, "transform2");
-    node_map_type((node_t*)root, NODE_TYPE_TRANSFORM, f);
-    node_delete(transform, transform_t);
-    // node_map((node_t*)root, f);
-    root_delete(root);
-
     app_init();
     app_run();
 
